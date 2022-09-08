b1cc4cdd61961d08c09e1e17954201477c8a56af
Emmanuel Molteni <beyondtheuknown@gmail.com>
2022-09-08 07:03:54 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const { infoCursos } = require('./datos/cursos.js');
+
+// Routers
+const routerProgramacion = require('./routers/programacion.js');
+app.use('/api/cursos/programacion',routerProgramacion);
+const routerMatematicas = require('./routers/matematicas.js');
+app.use('/api/cursos/matematicas',routerMatematicas);
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () => {
+  console.log(`listening in port ${PUERTO}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Mi primer servidor, Cursos 💻.');
+});
+
+app.get('/api/cursos', (req, res) => {
+  res.send(JSON.stringify(infoCursos));
+});
---
+++ routers/programacion.js
@@ -0,0 +1,91 @@
+const express = require('express');
+
+const { programacion } = require('../datos/cursos.js').infoCursos;
+
+const routerProgramacion = express.Router();
+
+// Middleware
+routerProgramacion.use(express.json());
+
+//Cursos de programacion
+routerProgramacion.get('/', (req, res) => {
+  res.send(JSON.stringify(programacion));
+});
+
+// Parametro url = ':lenguaje'
+routerProgramacion.get('/:lenguaje', (req, res) => {
+  const lenguaje = req.params.lenguaje;
+  const resultados = programacion.filter(curso => curso.lenguaje === lenguaje);
+
+  if(resultados.length === 0) {
+    return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+  }
+
+  //Parametros query "/x?ordenar=vistas"
+  if(req.query.ordenar === 'vistas') {
+    return res.send(JSON.stringify(resultados.sort((a, b) => b.vistas - a.vistas)));
+  }
+
+  res.send(JSON.stringify(resultados));
+});
+
+routerProgramacion.get('/:lenguaje/:nivel', (req, res) => {
+  const lenguaje = req.params.lenguaje;
+  const nivel = req.params.nivel;
+
+  const resultados = programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel == nivel);
+
+  if(resultados.length === 0) {
+    return res.status(404).send(`No se encontraron los cursos de ${lenguaje} de nivel ${nivel}`);
+  }
+  res.send(JSON.stringify(resultados));
+});
+
+routerProgramacion.post('/', (req, res) => {
+  let cursoNuevo = req.body;
+  programacion.push(cursoNuevo);
+  res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.put('/:id', (req, res) => {
+  const cursoActualizado = req.body;
+  const id = req.params.id;
+
+  const indice = programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    programacion[indice] = cursoActualizado;
+  } else {
+    res.status(404).send(`No se encontro el curso a actualizar`);
+  }
+  res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.patch('/:id', (req, res) => {
+  const infoActualizada = req.body;
+  const id = req.params.id;
+
+  const indice = programacion.findIndex(curso => curso.id == id);
+
+  if(indice >= 0) {
+    const cursoAModificar = programacion[indice];
+    Object.assign(cursoAModificar, infoActualizada);
+  } else {
+    res.status(404).send(`No se encontro el curso a modificar`);
+  }
+  res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const indice = programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    programacion.splice(indice, 1);
+  } else {
+    return res.status(404).send(`No se encontro el curso a borrar`);
+  }
+  res.send(JSON.stringify(programacion));
+});
+
+module.exports = routerProgramacion;
